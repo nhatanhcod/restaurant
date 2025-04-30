7370c11e577bc8cbbf7e4a5f8b9bf2fc9006f2c9
nhatanhcod <nhatanhz0811@gmail.com>
2025-04-30 14:52:55 +0700

    Update AccountDTO.java

--- src/DTO/AccountDTO.java
+++ src/DTO/AccountDTO.java
@@ -39,7 +39,7 @@ public class AccountDTO {
     public String getUsername() {
         return username;
     }
-
+    
     public void setUsername(String username) {
         this.username = username;
     }
